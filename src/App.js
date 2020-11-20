379e9efa2a332bf89ec4d3ab0e3aa793fc4be94e
Ryan Hull <ryandelonhull@gmail.com>
2020-11-19 20:54:30 -0500

    initial setup add components folders and files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
 import Search from "./components/Search";

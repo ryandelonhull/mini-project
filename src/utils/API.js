f3b9ec7622688f2d85afc7a8a72de48145782ab7
Ryan Hull <ryandelonhull@gmail.com>
2020-11-21 14:59:05 -0500

    finish index.js and App.js files

--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import axios from "axios";
-
-
-export default {
-    getBooks: function(search) {
-        return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + search)
-    }
-};

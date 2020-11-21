fd2c8893fdfab9d39c1c24084f9a1bb5fc958b21
Ryan Hull <ryandelonhull@gmail.com>
2020-11-21 15:23:02 -0500

    work on components

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="pull-right">
+        <i className="fab fa-github" /> React(ion) Library Lookup a Book Up Here
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,7 +12,7 @@ class Home extends Component {
   state = {
     books: [],
     q: "",
-    message: "Search For A Book To Begin!"
+    message: "What book would you like to lookup!"
   };
 
   handleInputChange = event => {
@@ -32,7 +32,7 @@ class Home extends Component {
       .catch(() =>
         this.setState({
           books: [],
-          message: "No New Books Found, Try a Different Query"
+          message: "No New Books Found, Try a Different Search"
         })
       );
   };
